19929be0641b278343ed13281ff2b304d97f59a3
jooho-le <wngh9696@naver.com>
2025-02-02 12:22:05 +0900

    2/2

---
+++ src/chap06/c05/CalculatorExample_static.java
@@ -0,0 +1,10 @@
+public class CalculatorExample_static {
+    public static void main(String[] args) {
+        // 정적멤버 사용 -> 클래스이름.메소드
+        double result1 = 10 * 10 * Calculator_static.pi;
+        int result2 = Calculator_static.plus(10, 5);
+        int result3 = Calculator_static.minus(10, 5);
+
+        System.out.println("result1: " + result1 + " result2: " + result2 + "result3: "+ result3);
+    }
+}
