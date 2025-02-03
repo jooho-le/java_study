ad7ff9d4558a48bf6abfb62dad6126605ac38d5b
jooho-le <wngh9696@naver.com>
2025-02-03 19:20:49 +0900

    2/3

---
+++ src/chap02/c03/ByteOperationExample.java
@@ -0,0 +1,13 @@
+package chap02.c03;
+
+public class ByteOperationExample {
+    public static void main(String[] args) {
+        byte result1 = 10 + 20;
+        System.err.println(result1);
+
+        byte x = 10;
+        byte y = 10;
+        int result2 = x + y;
+        System.out.println(result2);
+    }
+}
