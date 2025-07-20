96e88210f589e3c0dc0a16f3192f53e8797a0fff
jooho-le <wngh9696@naver.com>
2025-07-21 06:20:53 +0900

    수정본

--- src/chap06/c05/SingletonExample.java
+++ src/chap06/c05/SingletonExample.java
@@ -1,19 +1,19 @@
-﻿package chap06.c05;
+﻿// package chap06.c05;
 
-public class SingletonExample {
-    public static void main(String[] args) {
-        /*
-        Singleton obj1 = new Singleton();
-        Singleton obj2 = new Singleton();
-         */ // 싱글톤 패턴 구현할 때 보통 생성자를 private으로 선언하여 외부에서 인스턴스를 직접 생성하지 못해서 이렇게 코딩하면 오류남 
+// public class SingletonExample {
+//     public static void main(String[] args) {
+//         /*
+//         Singleton obj1 = new Singleton();
+//         Singleton obj2 = new Singleton();
+//          */ // 싱글톤 패턴 구현할 때 보통 생성자를 private으로 선언하여 외부에서 인스턴스를 직접 생성하지 못해서 이렇게 코딩하면 오류남 
 
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
+//         Singleton obj1 = Singleton.getInstance();
+//         Singleton obj2 = Singleton.getInstance();
 
-        if(obj1 == obj2) {
-            System.out.println("같은 Singleton 객체입니다");
-        } else {
-            System.out.println("다른 Singleton 객체입니다");
-        }
-    }
-}
+//         if(obj1 == obj2) {
+//             System.out.println("같은 Singleton 객체입니다");
+//         } else {
+//             System.out.println("다른 Singleton 객체입니다");
+//         }
+//     }
+// }
---
+++ src/chap06/c06/a.java
@@ -0,0 +1,5 @@
+package chap06.c06;
+
+public class A {
+    
+}
